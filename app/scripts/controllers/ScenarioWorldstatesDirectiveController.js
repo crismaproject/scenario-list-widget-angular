e54cb2fb19cfc3c3b232c68c52ab6742c152c64d
Daniel Meiers <daniel.meiers@cismet.de>
2014-06-06 11:21:41 +0200

    #2 implementation of the scenario list widget directive as html list

--- app/scripts/controllers/ScenarioWorldstatesDirectiveController.js
+++ app/scripts/controllers/ScenarioWorldstatesDirectiveController.js
@@ -1,66 +1,23 @@
 angular.module(
     'de.cismet.crisma.widgets.scenarioListWidget.controllers',
     [
-        'de.cismet.crisma.widgets.scenarioListWidget.services',
         'de.cismet.commons.angular.angularTools'
     ]
 ).controller(
     'de.cismet.crisma.widgets.scenarioListWidget.controllers.ScenarioWorldstatesDirectiveController',
     [
         '$scope',
-        '$injector',
-        'de.cismet.crisma.widgets.scenarioListWidget.services.ScenarioWorldstatesService',
-        'de.cismet.commons.angular.angularTools.AngularTools',
-        'DEBUG',
-
-        function ($scope, $injector, wsService, AngularTools, DEBUG) {
+        'de.cismet.crisma.ICMM.Worldstates',
+        function ($scope, Worldstates) {
             'use strict';
-
-            var initializeShareService;
-
-            // using initializer function as we don't need anything of this stuff in this scope
-            initializeShareService = function () {
-                var shareService;
-
-                if ($injector.has('de.cismet.crisma.widgets.shared.SharedService')) {
-                    if (DEBUG) {
-                        console.log('shareservice available, attaching selectedWorldstatesChanged listener');
-                    }
-
-                    shareService = $injector.get('de.cismet.crisma.widgets.shared.SharedService');
-
-                    $scope.$on('selectedWorldstatesChanged', function () {
-                        AngularTools.safeApply($scope, function () {
-                            $scope.selectedWorldstates = shareService.getSelectedWorldstates();
-                        });
-                    });
-
-                    $scope.$watch('selectedWorldstates', function (n, o) {
-                        if (typeof n === 'undefined') {
-                            throw {
-                                name: 'IllegalStateException',
-                                message: 'selected worldstate undefined'
-                            };
-                        }
-
-                        if (o === n) {
-                            // ignore event, do nothing
-                            return;
-                        }
-
-                        shareService.setSelectedWorldstates($scope.selectedWorldstates);
-                    });
-                } else {
-                    if (DEBUG) {
-                        console.log('shareservice not available, not listening for selectedWorldstatesChanged');
-                    }
+            var worldstateId;
+            $scope.$watch('selectedWorldstateNode', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    worldstateId = $scope.selectedWorldstateNode.objectKey;
+                    worldstateId = worldstateId.substring(worldstateId.lastIndexOf('/')+1,worldstateId.length);
+                    $scope.selectedWorldstate = Worldstates.get({wsId: worldstateId});
                 }
-            };
-
-            initializeShareService();
-
-            $scope.scenarioWorldstates = wsService.getScenarioWorldstates().query();
-            $scope.selectedWorldstates = [];
+            });
         }
     ]
 );
