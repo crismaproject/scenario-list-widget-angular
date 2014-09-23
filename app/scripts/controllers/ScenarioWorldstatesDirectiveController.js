501ec78ac48efe7ee84b7975dd9e3e9246ebf591
Daniel Meiers <daniel.meiers@cismet.de>
2014-09-23 09:34:02 +0200

    #7 add a watch for the selecteded worldstates
    since this watch changes the selectedWorldstateNode, this fires also the
    watch for the selectedWorldstateNode which changes the
    selectedWorldstate. To avoid a infinite loop of watch calls a flaf that
    indicates the change from the selectedWorldstate watch is used.

--- app/scripts/controllers/ScenarioWorldstatesDirectiveController.js
+++ app/scripts/controllers/ScenarioWorldstatesDirectiveController.js
@@ -1,18 +1,54 @@
 angular.module(
     'de.cismet.crisma.widgets.scenarioListWidget.controllers',
     [
-        'de.cismet.commons.angular.angularTools'
+        'de.cismet.commons.angular.angularTools',
+        'de.cismet.cids.rest.collidngNames.Nodes'
     ]
-    ).controller(
+).controller(
     'de.cismet.crisma.widgets.scenarioListWidget.controllers.ScenarioWorldstatesDirectiveController',
     [
         '$scope',
         'de.cismet.crisma.ICMM.Worldstates',
-        function ($scope, Worldstates) {
+        'de.cismet.collidingNameService.Nodes',
+        '$q',
+        function ($scope, Worldstates, Nodes, $q) {
             'use strict';
-            var worldstateId;
+            var worldstateId, changeFromSelectedWorldstateWatch;
+
+            function getNodeKeyForWorldstate (ws) {
+                var defer;
+                defer = $q.defer();
+                Worldstates.get({
+                    'wsId': ws.id,
+                    level: 100,
+                    field: 'parentworldstate,id',
+                    deduplicate: true},
+                function (parents) {
+                    var key;
+                    key = parents.id;
+                    while (parents.parentworldstate) {
+                        parents = parents.parentworldstate;
+                        key += '.' + parents.id
+                    }
+
+                    defer.resolve(key.split('.').reverse().join('.'));
+                });
+                return defer.promise;
+            }
+
+            /*
+             * The watch for the selected Worldstate node listens for changes of the selected node
+             * that occur when the user selects a list item.
+             * Furhtermore this watch is fires when the selectedWorldstate binding is changed from directives
+             * outer scope. In that case we must not update the selectedWorldstate again to avoid an infinte loop 
+             * of watch calls
+             */
             $scope.$watch('selectedWorldstateNode', function (newVal, oldVal) {
                 if (newVal !== oldVal) {
+                    if (changeFromSelectedWorldstateWatch) {
+                        changeFromSelectedWorldstateWatch = false;
+                        return;
+                    }
                     worldstateId = $scope.selectedWorldstateNode.objectKey;
                     worldstateId = worldstateId.substring(worldstateId.lastIndexOf('/') + 1, worldstateId.length);
                     Worldstates.get({wsId: worldstateId}, function (ws) {
@@ -20,6 +56,23 @@ angular.module(
                     });
                 }
             });
+
+            /*
+             * This watch listens for changes of the selectedWorldstate from directives outer scope.
+             * It is also fired when the selectedWorldstateNode has changed
+             */
+            $scope.$watch('selectedWorldstate', function (newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    getNodeKeyForWorldstate($scope.selectedWorldstate).then(function (key) {
+
+                        Nodes.get({nodeId: Nodes.utils.getRequestIdForNodeKey(key)}, function (wsNode) {
+                            changeFromSelectedWorldstateWatch=true;
+                            $scope.selectedWorldstateNode = wsNode;
+                        });
+                    });
+                }
+            });
+
         }
     ]
-    );
+);
